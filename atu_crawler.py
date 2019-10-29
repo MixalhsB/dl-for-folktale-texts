2f104901d232de3ba2eb2f56bcb9ba74526bacbf
Nicole <macher.nicole@gmail.com>
2019-10-29 14:09:16 +0100

    minimale und maximale märchenlängen werden berechnet und in eine Datei geschrieben

--- atu_crawler.py
+++ atu_crawler.py
@@ -2,6 +2,7 @@ from collections import defaultdict
 import os
 from nltk.tokenize import sent_tokenize
 from nltk.tokenize import word_tokenize
+import math
 
 class Story:
 
@@ -91,6 +92,13 @@ if __name__ == "__main__":
 	stupidogrelen = 0
 	jokeslen = 0
 	formulalen = 0
+	min_max_animallen = [math.inf,0]
+	min_max_magiclen = [math.inf,0]
+	min_max_religiouslen = [math.inf,0]
+	min_max_realisticlen = [math.inf,0]
+	min_max_stupidogrelen = [math.inf,0]
+	min_max_jokeslen = [math.inf,0]
+	min_max_formulalen = [math.inf,0]
 	# dictionary that saves average sentence lengths per tale type and language
 	sentence_length_average = {}
 	sentence_length_average["animaltales"] = {}
@@ -135,30 +143,59 @@ if __name__ == "__main__":
 
 					animaltales[language].append(story)
 					animallen += len(story.text.split())
+					if len(story.text.split()) < min_max_animallen[0]: # neues minimum
+						min_max_animallen[0] = len(story.text.split())
+					if len(story.text.split()) > min_max_animallen[1]: # neues maximum
+						min_max_animallen[1] = len(story.text.split())
+
 
 				elif 300 <= atu <= 749:
 					magictales[language].append(story)
 					magiclen += len(story.text.split())
+					if len(story.text.split()) < min_max_magiclen[0]: # neues minimum
+						min_max_magiclen[0] = len(story.text.split())
+					if len(story.text.split()) > min_max_magiclen[1]: # neues maximum
+						min_max_magiclen[1] = len(story.text.split())
 
 				elif 750 <= atu <= 849:
 					religioustales[language].append(story)
 					religiouslen += len(story.text.split())
+					if len(story.text.split()) < min_max_religiouslen[0]: # neues minimum
+						min_max_religiouslen[0] = len(story.text.split())
+					if len(story.text.split()) > min_max_religiouslen[1]: # neues maximum
+						min_max_religiouslen[1] = len(story.text.split())
 
 				elif 850 <= atu <= 999:
 					realistictales[language].append(story)
 					realisticlen += len(story.text.split())
+					if len(story.text.split()) < min_max_realisticlen[0]: # neues minimum
+						min_max_realisticlen[0] = len(story.text.split())
+					if len(story.text.split()) > min_max_realisticlen[1]: # neues maximum
+						min_max_realisticlen[1] = len(story.text.split())
 
 				elif 1000 <= atu <= 1199:
 					stupidogre[language].append(story)
 					stupidogrelen += len(story.text.split())
+					if len(story.text.split()) < min_max_stupidogrelen[0]: # neues minimum
+						min_max_stupidogrelen[0] = len(story.text.split())
+					if len(story.text.split()) > min_max_stupidogrelen[1]: # neues maximum
+						min_max_stupidogrelen[1] = len(story.text.split())
 
 				elif 1200 <= atu <= 1999:
 					jokes[language].append(story)
 					jokeslen += len(story.text.split())
+					if len(story.text.split()) < min_max_jokeslen[0]: # neues minimum
+						min_max_jokeslen[0] = len(story.text.split())
+					if len(story.text.split()) > min_max_jokeslen[1]: # neues maximum
+						min_max_jokeslen[1] = len(story.text.split())
 
 				elif 2000 <= atu <= 2399:
 					formulatales[language].append(story)
 					formulalen += len(story.text.split())
+					if len(story.text.split()) < min_max_formulalen[0]: # neues minimum
+						min_max_formulalen[0] = len(story.text.split())
+					if len(story.text.split()) > min_max_formulalen[1]: # neues maximum
+						min_max_formulalen[1] = len(story.text.split())
 
 		dirName = 'clean'
 		if not os.path.exists(dirName):
@@ -235,6 +272,15 @@ if __name__ == "__main__":
 		g.write(str(sentence_length_average))
 	with open("average_title_length.txt", "w+", encoding="utf-8") as h:
 		h.write(str(title_length_average))
+
+	with open("min_max_tale_length.txt", "w+", encoding="utf-8") as file:
+		file.write("animaltales" + "\t" + " ".join(map(str,min_max_animallen)) + "\n")
+		file.write("magictales" + "\t" + " ".join(map(str,min_max_magiclen)) + "\n")
+		file.write("religioustales" + "\t" + " ".join(map(str,min_max_religiouslen)) + "\n")
+		file.write("realistictales" + "\t" + " ".join(map(str,min_max_realisticlen)) + "\n")
+		file.write("stupidogretales" + "\t" + " ".join(map(str, min_max_stupidogrelen)) + "\n")
+		file.write("jokes" + "\t" + " ".join(map(str, min_max_jokeslen)) + "\n")
+		file.write("formulatales" + "\t" + " ".join(map(str, min_max_formulalen)) + "\n")
 
 		# print("Number of Folktales without ATU: " + str(unknowncounter))
 		# print("Number of Folktales in total: " + str(i))
