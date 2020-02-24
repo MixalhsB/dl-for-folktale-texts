1dd4fa1765862efbbaeb4c852cc1f569110fbcbc
lisakluge <lisa.kluge99@web.de>
2020-02-24 11:42:53 +0100

    Finale Änderungen in der translate-Datei, sodass nun nach Bedarf alle Märchen eines Korpus zu den Trainingsdaten eines anderen Korpus hinzugefügt werden können und übersetzte Märchen auch abgespeichert werden. Beides sind zwei neue Methoden.

--- classification/translate.py
+++ classification/translate.py
@@ -1,6 +1,9 @@
 from bs4 import BeautifulSoup
 from json.decoder import JSONDecodeError
 import googletrans
+import os.path
+from corpus import Corpus
+from classifier import *
 
 
 # How many tales do two corpora in different languages have in common?
@@ -77,9 +80,116 @@ def extract_translate_unique_tales(ground_corpus, other_corpus):
         print("\nThe ground corpus was extended from a total number of "
               + str(previous_len) + " tales to now " + str(previous_len + n)
               + " tales in total.\n")
+        
+def translate_tales_for_training_data(ground_corpus, other_corpus):
+    ''' Takes two Corpus-objects and a filename as input, first one is the main corpus,
+    the second one will be used to expand the first one. ALL tales
+    in the other corpus will be added to the TRAINING stories of the ground corpus.
+    '''
+    previous_len = len(ground_corpus.train_stories)
 
-##German = Corpus('../corpora.dict', 'German', seed=123, binary_mode=True)
-##English = Corpus('../corpora.dict', 'English', seed=123, binary_mode=True)
+    ground_language = ground_corpus.language
+    other_language = other_corpus.language
+    
+
+    other_stories = {story for story in other_corpus.stories}
+    assert other_stories != []
+    
+    n = 0
+        
+    for story in other_stories:
+        tale = BeautifulSoup(story[4], "html.parser").text
+        title = BeautifulSoup(story[0], "html.parser").text
+        
+        try:
+            translator = googletrans.Translator()
+            print(title)
+            trans_tale = translator.translate(tale, src=other_language, dest=ground_language).text
+            trans_title = translator.translate(title, src=other_language, dest=ground_language).text
+            print(title + ' -> ' + trans_title)
+            new_story = trans_title, story[1], story[2], story[3], trans_tale
+            ground_corpus.train_stories.append(new_story)
+            n += 1
+        except JSONDecodeError:
+            print('error while trying to connect to Google Translate')
+            pass
+
+    if len(other_stories) > 0:
+        print("\nThe ground corpus was extended from a total number of "
+              + str(previous_len) + " tales to now " + str(previous_len + n)
+              + " tales in total.\n")
+
+        
+def translate_save_tales(ground_corpus, other_corpus):
+    ''' Takes two Corpus-objects and a filename as input, first one is the main corpus,
+    the second one will be used to expand the first one. ALL tales
+    in the other corpus will be added to the TRAINING stories of the ground corpus.
+    After method has been called on language pair once, result is saved in a file. For every other
+    call, the data will be read from the file. 
+    '''
+    
+    previous_len = len(ground_corpus.train_stories)
+
+    ground_language = ground_corpus.language
+    other_language = other_corpus.language
+
+    other_stories = {story for story in other_corpus.stories}
+    assert other_stories != []
+    
+    save_file = str(ground_language) + "_" + str(other_language) + ".txt"
+    
+    if os.path.isfile(save_file):
+        with open(save_file) as s_f:
+            n=0
+            for line in s_f:
+                story = line.split("\t")
+                ground_corpus.train_stories.append(story)
+                n+=1
+            
+
+
+    else:
+        with open(save_file, 'w', encoding= 'utf-8') as s_f:
+            
+            n = 0
+                
+            for story in other_stories:
+                tale = BeautifulSoup(story[4], "html.parser").text
+                title = BeautifulSoup(story[0], "html.parser").text
+
+                try:
+                    translator = googletrans.Translator()
+                    print(title)
+                    trans_tale = translator.translate(tale, src=other_language, dest=ground_language).text
+                    trans_title = translator.translate(title, src=other_language, dest=ground_language).text
+                    print(title + ' -> ' + trans_title)
+                    new_story = trans_title, story[1], story[2], story[3], trans_tale
+                    ground_corpus.train_stories.append(new_story)
+                    n += 1
+
+                    s_f.write(str(trans_title) + "\t" +
+                              str(story[1]) + "\t" +
+                              str(story[2]) + "\t" +
+                              str(story[3]) + "\t" +
+                              str(trans_tale) + "\n")
+                        
+                except JSONDecodeError:
+                    print('error while trying to connect to Google Translate')
+                    pass
+
+    if len(other_stories) > 0:
+        print("\nThe ground corpus was extended from a total number of "
+                + str(previous_len) + " tales to now " + str(previous_len + n)
+                 + " tales in total " + "\n")
+
+German = Corpus('../corpora.dict', 'German', seed=123, binary_mode=True)
+English = Corpus('../corpora.dict', 'English', seed=123, binary_mode=True)
+
+#translate_tales_for_training_data(English,German)
+#print("hi")
+#translate_save_tales(English,German)
+#print("hello")
+
 ##French = Corpus('../corpora.dict', 'French', seed=123, binary_mode=True)
 ##Dutch = Corpus('../corpora.dict', 'Dutch', seed=123, binary_mode=True)
 ##Spanish = Corpus('../corpora.dict', 'Spanish', seed=123, binary_mode=True)
