acc6a681bd0450f7797ce0e65c91f7ac5de33bed
Michael Vrazitulis <s8mivraz@stud.uni-saarland.de>
2020-02-25 19:55:38 +0100

    Option, bei der Evaluation ueber mehreren Runs den Durchschnitt zu nehmen, hinzugefuegt.

--- classification/evaluator.py
+++ classification/evaluator.py
@@ -4,22 +4,17 @@ from sklearn import metrics
 
 
 class Evaluator:
-    def __init__(self, corpus, classify):
-        self.corpus = corpus
-        self.classify = classify
+    def __init__(self, list_of_corpora, list_of_classify_instances):
+        self.list_of_corpora = list_of_corpora
+        self.list_of_classify_instances = list_of_classify_instances
+        assert len(self.list_of_corpora) == len(self.list_of_classify_instances)
 
     def evaluate(self):
-        predicted = []
-        true = []
-        '''
-        for story in self.corpus.test_stories:
-            predicted_class_name = self.classify(story[4])
-            true_class_name = self.corpus.get_gold_class_name(story)
-            predicted.append(predicted_class_name)
-            true.append(true_class_name)
-        '''
-        predicted_class_names = self.classify([story[4] for story in self.corpus.test_stories])
-        true_class_names = [self.corpus.get_gold_class_name(story) for story in self.corpus.test_stories]
+        predicted_class_names, true_class_names = [], []
+        for i, (current_corpus, current_classify_instance) in enumerate(zip(self.list_of_corpora, self.list_of_classify_instances)):
+            print('\nRUN NUMBER ' + str(i + 1) + ':\n')
+            predicted_class_names += current_classify_instance([story[4] for story in current_corpus.test_stories])
+            true_class_names += [current_corpus.get_gold_class_name(story) for story in current_corpus.test_stories]
         print('\nTrue class names:\n', true_class_names, '\n')
         print('\nPredicted class names:\n', predicted_class_names, '\n')
 
--- classification/test_classification.py
+++ classification/test_classification.py
@@ -18,33 +18,28 @@ if __name__ == '__main__':
         exclude_sw_string = input('Exclude stopwords (y/n)? ')
     while binary_mode_string.lower() not in ('y', 'n'):
         binary_mode_string = input('Binary classification (y) or multi-class (n)? ')
-    # while not number_of_runs.isdigit() or int(number_of_runs) < 1:
-    #     number_of_runs = input('Enter number of runs: ')
+    while not number_of_runs.isdigit() or int(number_of_runs) < 1:
+        number_of_runs = input('Enter number of runs: ')
     print()
     
     exclude_stop_words = True if exclude_sw_string == 'y' else False
     binary_mode = True if binary_mode_string == 'y' else False
     
-    ground_corpus = corpus.Corpus('../corpora.dict', language.lower().capitalize(), seed=None,
-                                  exclude_stop_words=exclude_stop_words, binary_mode=binary_mode)
+    list_of_corpora = []
+    list_of_classifiers = []
     
-    '''
-    if add_translated == 'y':
-        for other_lang in (lang for lang in languages_lower if lang != language):
-            other_corpus = corpus.Corpus('../corpora.dict', other_lang.capitalize())
-            translate.extract_translate_unique_tales(ground_corpus, other_corpus)
-        ground_corpus.shuffle_stories_and_split_them()
-        ground_corpus.w2i_dict = ground_corpus.get_word_to_index_dict()
-    '''
+    for i in range(int(number_of_runs)):
+        list_of_corpora.append(corpus.Corpus('../corpora.dict', language.lower().capitalize(), seed=None,
+                                             exclude_stop_words=exclude_stop_words, binary_mode=binary_mode))
+        list_of_classifiers.append(classifier.Classifier(list_of_corpora[i]))
+        
+    eval_dumb = evaluator.Evaluator(list_of_corpora, [clsf.dumb_classify for clsf in list_of_classifiers])
+    eval_length = evaluator.Evaluator(list_of_corpora, [clsf.length_classify for clsf in list_of_classifiers])
+    eval_reuters = evaluator.Evaluator(list_of_corpora, [clsf.simple_reuters_classify for clsf in list_of_classifiers])
+    eval_book = evaluator.Evaluator(list_of_corpora, [clsf.book_inspired_classify for clsf in list_of_classifiers])
+    eval_doc2vec = evaluator.Evaluator(list_of_corpora, [clsf.doc2vec_classify for clsf in list_of_classifiers])
+    eval_ngram = evaluator.Evaluator(list_of_corpora, [clsf.ngram_classify for clsf in list_of_classifiers])
     
-    clsf = classifier.Classifier(ground_corpus)
-    eval_dumb = evaluator.Evaluator(ground_corpus, clsf.dumb_classify)
-    eval_length = evaluator.Evaluator(ground_corpus, clsf.length_classify)
-    eval_reuters = evaluator.Evaluator(ground_corpus, clsf.simple_reuters_classify)
-    eval_book = evaluator.Evaluator(ground_corpus, clsf.book_inspired_classify)
-    eval_doc2vec = evaluator.Evaluator(ground_corpus, clsf.doc2vec_classify)
-    eval_ngram = evaluator.Evaluator(ground_corpus, clsf.ngram_classify)
-
     resulting_string = '\nDUMB:\n'
     resulting_string += eval_dumb.evaluate() + '\n'
     print('\nFinished computing DUMB classification.\n')
@@ -52,11 +47,11 @@ if __name__ == '__main__':
     resulting_string += '\nLENGTH:\n'
     resulting_string += eval_length.evaluate() + '\n'
     print('\nFinished computing LENGTH classification.\n')
-
+    
     resulting_string += '\nREUTERS:\n'
     resulting_string += eval_reuters.evaluate() + '\n'
     print('\nFinished computing REUTERS classification.\n')
-
+    
     resulting_string += '\nBOOK:\n'
     resulting_string += eval_book.evaluate() + '\n'
     print('\nFinished computing BOOK classification.\n')
@@ -68,5 +63,5 @@ if __name__ == '__main__':
     resulting_string += '\nN-GRAM:\n'
     resulting_string += eval_ngram.evaluate()
     print('\nFinished computing N-GRAM classification.\n')
-
+    
     print(resulting_string)
